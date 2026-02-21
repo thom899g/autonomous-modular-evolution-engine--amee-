f062152e11bb7b71dd064ffa9433dac37dddc4e9
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-21 13:23:31 +0100

    Automated commit by HiveMind

---
+++ amee/core/__init__.py
@@ -0,0 +1,5 @@
+"""
+Core components of the Autonomous Modular Evolution Engine (AMEE).
+
+This package contains the fundamental modules that power AMEE's ability to dynamically optimize and evolve within an ecosystem.
+"""
